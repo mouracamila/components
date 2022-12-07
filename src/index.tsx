5df60278810361c1f52e7adfaecc043dac81923c
Camila Moura <camilapaleo@gmail.com>
2022-12-07 18:58:31 +0100

    add index.html

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter as Router } from "react-router-dom";
+
 
 import './index.css';
 import App from './App';
@@ -9,9 +11,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+  <App />
+</Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
